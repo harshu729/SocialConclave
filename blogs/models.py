f2c99fbdbe375af54863630317cbdaea9e9d3bed
Harshit Sharma <harshu729@gmail.com>
2021-03-05 23:49:53 +0530

    basic changes

--- blogs/models.py
+++ blogs/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+
 class Blog(models.Model):
-    content = models.TextField(blank=True)
-    title = models.CharField(max_length=2000000000,blank=True)
-    slug = models.CharField(max_length=2000000000,blank=True)
-    time = models.DateTimeField(default=timezone.localtime)
-    
+    background = models.ImageField(
+        upload_to="gallery", default="", blank=False)
+    title = models.CharField(max_length=2000000000,  default="", blank=False)
+    content = models.TextField(default="", blank=False)
+    image1 = models.ImageField(upload_to="gallery", blank=True)
+    quote = models.CharField(max_length=2000000000, blank=True, default="")
+    content_2 = models.TextField(blank=True, default="")
+    image2 = models.ImageField(upload_to="gallery", blank=True)
+    content_3 = models.TextField(blank=True, default="")
+    slug = models.CharField(max_length=200, default="", blank=False)
+    time = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
-        return self.title + f' [{self.time}]'
+        return self.title + " : " + self.slug + f' [{self.time}]'
--- register/models.py
+++ register/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from phone_field import PhoneField
+from django.core import validators
 
 # Create your models here.
+
+
+class Delegate(models.Model):
+    topic = (
+        (0, 'CLIMATE ACTION'),
+        (1, 'FINANCIAL INDEPENDENCE OF WOMEN'),
+        (2, 'RURAL ILLITERACY AND UNEMPLOYMENT'),
+        (3, 'YOUTH PRIVACY AND SOCIAL MEDIA'),
+        (4, 'ZERO HUNGER'),
+    )
+    genderChoices = (
+        (1, 'Male'),
+        (2, 'Female'),
+        (3, 'Other'),
+    )
+    name = models.CharField(max_length=200, blank=True)
+    gender = models.CharField(choices=genderChoices, max_length=200)
+    address = models.TextField(blank=True)
+    phoneNumber = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=200,  blank=True)
+    topicPref1 = models.CharField(
+        max_length=2000, choices=topic,  blank=True, default=None)
+    topicPref2 = models.CharField(
+        max_length=2000, choices=topic,  blank=True, default=None)
+    topicPref3 = models.CharField(
+        max_length=2000, choices=topic,  blank=True, default=None)
+    topicPref4 = models.CharField(
+        max_length=2000, choices=topic,  blank=True, default=None)
+    topicPref5 = models.CharField(
+        max_length=2000, choices=topic,  blank=True, default=None)
+    age = models.IntegerField(validators=[validators.MinValueValidator(
+        16), validators.MaxValueValidator(21)], null=True)
+
+    schoolName = models.CharField(max_length=1000, blank=True)
+    courseName = models.CharField(max_length=1000, blank=True)
+    yearGrad = models.IntegerField(validators=[validators.MinValueValidator(
+        999), validators.MaxValueValidator(10000)], blank=True)
+    city = models.CharField(max_length=1000, blank=True)
